f5f6018c70f6ad11145a0eff5a0f8ffa362cd853
None <None>
2023-05-26 15:54:56 -0300

    fix a mistake with variables

--- switchTarget.py
+++ switchTarget.py
@@ -7,11 +7,11 @@ import globalVariables
 
 def change_target():
     while True:
-        if globalVariables.switch_target_on and globalVariables.switch_target_on:
+        if globalVariables.switch_target_on and globalVariables.switch_target_key:
 
             time.sleep(random.randint(4, 5))
 
-            windowsAPI.send_keystrokes(virtualKeys.vk_code.get(globalVariables.switch_target_on))
+            windowsAPI.send_keystrokes(virtualKeys.vk_code.get(globalVariables.switch_target_key))
 
             time.sleep(random.randint(0, 180))
 
