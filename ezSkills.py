f228d31bab0d3d1994dc3d83520bfca00819d4d1
None <None>
2023-05-27 00:09:05 -0300

    tweaks on UI

--- ezSkills.py
+++ ezSkills.py
@@ -1,4 +1,4 @@
-from tkinter import Entry, Tk, X, LEFT, RIGHT, Checkbutton, IntVar, LabelFrame, Label, Button, END, StringVar
+from tkinter import Entry, Tk, X, LEFT, RIGHT, Checkbutton, IntVar, LabelFrame, Label, Button, END, StringVar, W
 import os
 import globalVariables
 import initiateBot
@@ -51,8 +51,8 @@ var_target = IntVar()
 var_food = IntVar()
 var_health = IntVar()
 
-window_width = 336
-window_height = 130
+window_width = 350
+window_height = 165
 
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
@@ -60,59 +60,59 @@ screen_height = root.winfo_screenheight()
 x = (screen_width / 2) - (window_width / 2)
 y = (screen_height / 2) - (window_height / 2)
 
-root.geometry("336x130+" + str(int(x)) + "+" + str(int(y)))
+root.geometry("350x165+" + str(int(x)) + "+" + str(int(y)))
 root.title("ezSkills")
 if os.path.isfile("icon/pepeez.ico"):
     root.iconbitmap("icon/pepeez.ico")
 root.resizable(False, False)
 
 frame_char_name = LabelFrame(root, text="Character Name")
-frame_char_name.pack(fill=X, padx=1, pady=1)
+frame_char_name.pack(fill=X, padx=2, pady=2)
 
 entry_char_name = Entry(frame_char_name, textvariable=entry_var_char_name)
-entry_char_name.pack(fill=X, padx=1, pady=1)
+entry_char_name.pack(fill=X, padx=5, pady=5)
 
 frame_toggles = LabelFrame(root)
-frame_toggles.pack(fill=X, padx=1, pady=1)
+frame_toggles.pack(fill=X, padx=2, pady=2)
 
 check_target = Checkbutton(frame_toggles, text="Switch Target", variable=var_target, onvalue=1, offvalue=0,
                            command=get_checkbutton_state)
-check_target.pack(side=LEFT)
+check_target.pack(side=LEFT, padx=5, pady=5)
 
 check_f1 = Checkbutton(frame_toggles, text="Use Food", variable=var_food, onvalue=1, offvalue=0,
                        command=get_checkbutton_state)
-check_f1.pack(side=LEFT)
+check_f1.pack(side=LEFT, padx=5, pady=5)
 
 check_f2 = Checkbutton(frame_toggles, text="Use Healing Spell", variable=var_health, onvalue=1, offvalue=0,
                        command=get_checkbutton_state)
-check_f2.pack(side=LEFT)
+check_f2.pack(side=LEFT, padx=5, pady=5)
 
 frame_hotkeys = LabelFrame(root)
-frame_hotkeys.pack(fill=X, padx=1, pady=1)
+frame_hotkeys.pack(fill=X, padx=2, pady=2)
 
-label_switch_target_hotkey = Label(frame_hotkeys, text="Target Key:", width=10)
-label_switch_target_hotkey.pack(side=LEFT)
+label_switch_target_hotkey = Label(frame_hotkeys, text="Target Key:", width=9, anchor=W)
+label_switch_target_hotkey.pack(side=LEFT, pady=5)
 
 entry_switch_target_hotkey = Entry(frame_hotkeys, width=5, textvariable=entry_var_target)
-entry_switch_target_hotkey.pack(side=LEFT)
+entry_switch_target_hotkey.pack(side=LEFT, pady=5)
 
-label_food_hotkey = Label(frame_hotkeys, text="Food Key:", width=10)
-label_food_hotkey.pack(side=LEFT)
+label_food_hotkey = Label(frame_hotkeys, text="Food Key:", width=8, anchor=W)
+label_food_hotkey.pack(side=LEFT, pady=5)
 
 entry_food_hotkey = Entry(frame_hotkeys, width=5, textvariable=entry_var_food)
-entry_food_hotkey.pack(side=LEFT)
+entry_food_hotkey.pack(side=LEFT, pady=5)
 
-label_health_hotkey = Label(frame_hotkeys, text="Health Key:", width=10)
-label_health_hotkey.pack(side=LEFT)
+label_health_hotkey = Label(frame_hotkeys, text="Healing Spell Key:", width=14, anchor=W)
+label_health_hotkey.pack(side=LEFT, pady=5)
 
 entry_health_hotkey = Entry(frame_hotkeys, width=5, textvariable=entry_var_heal)
-entry_health_hotkey.pack(side=LEFT)
+entry_health_hotkey.pack(side=LEFT, pady=5)
 
 button_update_values = Button(root, text="Save", width=10, command=update_values)
-button_update_values.pack(side=RIGHT, padx=1, pady=1)
+button_update_values.pack(side=RIGHT, padx=2, pady=2)
 
 label_save_config = Label(root, text="Configuration not saved.")
-label_save_config.pack(side=LEFT, padx=1, pady=1)
+label_save_config.pack(side=LEFT, padx=2, pady=2)
 
 entry_var_char_name.trace("w", on_entry_change)
 entry_var_target.trace("w", on_entry_change)
